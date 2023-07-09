a5b8a4355461a6e63016a2cec63610f063d7a43c
Joesynth <133770468+Joesynth@users.noreply.github.com>
2023-07-09 07:49:06 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,33 @@
+//alert("oi");
+
+window.onload = () => {
+  const audio = document.getElementById("BackgroundM");
+
+  audio.addEventListener("progress", () => {
+    const duration = audio.duration;
+    if (duration > 0) {
+      for (let i = 0; i < audio.buffered.length; i++) {
+        if (
+          audio.buffered.start(audio.buffered.length - 1 - i) <
+          audio.currentTime
+        ) {
+          document.getElementById("buffered-amount").style.width = `${
+            (audio.buffered.end(audio.buffered.length - 1 - i) * 100) / duration
+          }%`;
+          break;
+        }
+      }
+    }
+  });
+
+  audio.addEventListener("timeupdate", () => {
+    const duration = audio.duration;
+    if (duration > 0) {
+      document.getElementById("progress-amount").style.width = `${
+        (audio.currentTime / duration) * 100
+      }%`;
+    }
+  });
+};
+
+
