1b57a05c76bfa753342484999fe89ee72379195d
Joesynth <133770468+Joesynth@users.noreply.github.com>
2023-07-27 09:20:32 -0300

    Add files via upload

---
+++ js/template.js
@@ -0,0 +1,22 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+<aside id="btEs">
+	<a href="newA.html"><div id="#" class="bt"><span>🌺️</span></div></a>
+	<a href="roPr.html"><div id="#" class="bt"><span>🎛️</span></div></a>
+	<a href="smoo.html"><div id="#" class="bt"><span>🎷️</span></div></a>
+	<a href="cont.html"><div id="#" class="bt"><span>⚡</span></div></a>
+	<a href="elet.html"><div id="#" class="bt"><span>💡️</span></div></a>
+</aside>
+
+<aside id="btE">
+	<a href="clas.html"><div id="#" class="bt"><span>🎻️</span></div></a>	
+	<a href="blue.html"><div id="#" class="bt"><span>🎺️</span></div></a>
+	<a href="chor.html"><div id="#" class="bt"><span id="bt2">🏖️</span></div></a>
+	<a href="funk.html"><div id="#" class="bt"><span id="bt2">🔌️</span></div></a>
+	<a href="game.html"><div id="#" class="bt"><span id="bt2">🎮️</span></div></a>
+	<a href="soun.html"><div id="#" class="bt"><span id="bt2" class="teste">📺️</span></div></a>
+</aside>
+`;
+
+document.body.appendChild(template.content);
