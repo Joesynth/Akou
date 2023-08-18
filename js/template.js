35f8e809b004c7861fa7f537bb7fd6492528d920
Joesynth <133770468+Joesynth@users.noreply.github.com>
2023-08-18 15:21:31 -0300

    Add files via upload

--- js/template.js
+++ js/template.js
@@ -7,16 +7,16 @@ template.innerHTML = `
 		<a href="smoo.html"><div id="#" class="bt"><span>🎷️</span></div></a>
 		<a href="cont.html"><div id="#" class="bt"><span>⚡</span></div></a>
 		<a href="elet.html"><div id="#" class="bt"><span>💡️</span></div></a>
-		<a href="viol.html"><div id="#" class="bt"><span>🎸️</span></div></a>
+		<a href="game.html"><div id="#" class="bt"><span>🎮️</span></div></a>
 	</aside>
 	<aside id="btE">
-		<a href="clas.html"><div id="#" class="bt"><span>🎻️</span></div></a>	
-		<a href="blue.html"><div id="#" class="bt"><span>🎺️</span></div></a>
-		<a href="pian.html"><div id="#" class="bt"><span>🎹</span></div></a>
+		<a href="clas.html"><div id="#" class="bt"><span id="bt2">🎻️</span></div></a>	
+		<a href="blue.html"><div id="#" class="bt"><span id="bt2">🎺️</span></div></a>
+		<a href="pian.html"><div id="#" class="bt"><span id="bt2">🎹</span></div></a>
 		<a href="chor.html"><div id="#" class="bt"><span id="bt2">🏖️</span></div></a>
+		<a href="viol.html"><div id="#" class="bt"><span id="bt2">🎸️</span></div></a>
 		<a href="funk.html"><div id="#" class="bt"><span id="bt2">🔌️</span></div></a>
-		<a href="game.html"><div id="#" class="bt"><span id="bt2">🎮️</span></div></a>
-		<a href="soun.html"><div id="#" class="bt"><span id="bt2" class="teste">📺️</span></div></a>
+		<a href="soun.html"><div id="#" class="bt"><span id="bt2">📺️</span></div></a>
 	</aside>
 	<details>
 		<summary><img id="logo" src="icon.png" class="bt"></summary>
