d0e23defd95d8781fc445018dc146cde00f8fddc
Joesynth <133770468+Joesynth@users.noreply.github.com>
2023-09-23 20:35:56 -0300

    Add files via upload

---
+++ js/cordel.js
@@ -0,0 +1,18 @@
+
+/*
+Merlânio Maia cordelistas
+Claudete Gomes cordelistas
+Cristine Nobre Leite cordelistas
+Sheilla Virgínia cordelistas
+Annecy Venâncio cordelistas
+Raniery Abrantes cordelistas
+Emanoel Oliveira cordelistas
+Manoel Belisario cordelistas
+Felipe Emmanuel cordelistas
+*/
+
+
+
+
+
+
